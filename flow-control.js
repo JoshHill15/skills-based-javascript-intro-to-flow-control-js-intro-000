9b76df0a261044503e4110ec8a4a2a43e47c93c5
Joshua Hill <josh_hill15@me.com>
2019-06-28 00:45:58 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,6 +1,12 @@
 function basicTeenager(age) {
-
+  if (age < 19 && > 13) {
+    return "You are a teenager!"
+  } else {
+    return "You are not a teenager!"
+basiTeenager(14)
+basicTeenager(15)
 }
+
 
 function teenager(age) {
 
